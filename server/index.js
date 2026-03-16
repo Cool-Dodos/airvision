e45fa11216c09ac3ad2822fa2194cb4256aa4391
Sid <siddharth.badjate1211@gmal.com>
2026-03-16 12:51:40 +0530

    restrict CORS to vercel domain

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const aqiRoutes = require('./routes/aqi');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://airvision-seven.vercel.app',
+    'http://localhost:4200'
+  ]
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
